e3bfd2b1a5d78e67055680af4886934b9fa742d0
Salies <serezane@protonmail.com>
2018-11-24 23:44:22 -0200

    Set index to use minified file

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>Steam Profiler</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,300" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" media="screen" href="index.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="index.min.css" />
 </head>
 <body>
 <?php
